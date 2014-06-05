c065e31eb0bd951a1fca73c86cd31b98563e033c
Denis Kormalev <denis.kormalev@opensoftdev.com>
2014-06-05 13:09:15 +0400

    ObjectsWeakCache added to core

---
+++ include/proofcore/objectsweakcache.h
@@ -0,0 +1,43 @@
+#ifndef OBJECTSWEAKCACHE_H
+#define OBJECTSWEAKCACHE_H
+
+#include <QHash>
+#include <QSharedPointer>
+#include <QWeakPointer>
+
+template <class Key, class T>
+class ObjectsWeakCache
+{
+public:
+    static ObjectsWeakCache<Key, T> &instance()
+    {
+        static ObjectsWeakCache<Key, T> inst;
+        return inst;
+    }
+
+
+    void add(Key key, QSharedPointer<T> object)
+    {
+        if (!object)
+            return;
+        m_cache[key] = object.toWeakRef();
+    }
+
+    QSharedPointer<T> value(Key key)
+    {
+        if (!m_cache.contains(key))
+            return QSharedPointer<T>();
+        QSharedPointer<T> shared = m_cache[key].toStrongRef();
+        if (!shared)
+            m_cache.remove(key);
+        return shared;
+    }
+
+private:
+    ObjectsWeakCache() {}
+    Q_DISABLE_COPY(ObjectsWeakCache)
+
+    QHash<Key, QWeakPointer<T>> m_cache;
+};
+
+#endif // OBJECTSWEAKCACHE_H
