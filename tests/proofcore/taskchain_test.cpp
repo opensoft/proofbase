1c9c8115c63a4119847e329459b1364fd7d7fc72
Denis Kormalev <denis.kormalev@opensoftdev.com>
2014-07-21 11:52:21 +0400

    more task chain tests

--- tests/proofcore/taskchain_test.cpp
+++ tests/proofcore/taskchain_test.cpp
@@ -3,24 +3,11 @@
 #include "proofcore/taskchain.h"
 
 #include <QThread>
+#include <QTimer>
 
 using namespace Proof;
-using testing::Test;
 
-class TaskChainTest: public Test
-{
-public:
-    TaskChainTest()
-    {
-    }
-
-protected:
-    void SetUp() override
-    {
-    }
-};
-
-TEST_F(TaskChainTest, memoryCheckWithNoChainCapture)
+TEST(TaskChainTest, memoryCheckWithNoChainCapture)
 {
     TaskChainWP chainWeak;
     QThread thread;
@@ -31,11 +18,10 @@ TEST_F(TaskChainTest, memoryCheckWithNoChainCapture)
         chain->moveToThread(&thread);
         chainWeak = chain.toWeakRef();
         auto task = TaskChain::createTask<>();
-        *task = [&flag](){
+        *task = [&flag]() {
             while (!flag) {}
         };
         chain->addTask(*task);
-        chain.clear();
     }
     EXPECT_FALSE(chainWeak.isNull());
     flag = true;
@@ -49,3 +35,111 @@ TEST_F(TaskChainTest, memoryCheckWithNoChainCapture)
     EXPECT_TRUE(chainWeak.isNull());
 }
 
+TEST(TaskChainTest, memoryCheckWithChainCapture)
+{
+    TaskChainWP chainWeak;
+    QWeakPointer<std::function<void()>> secondTaskWeak;
+    QThread thread;
+    thread.start();
+    std::atomic<bool> flag(false);
+    {
+        TaskChainSP chain = TaskChain::createChain();
+        chain->moveToThread(&thread);
+        chainWeak = chain.toWeakRef();
+        auto firstTask = TaskChain::createTask<>();
+        auto secondTask = TaskChain::createTask<>();
+        *firstTask = [&flag, chain, secondTask]() {
+            while (!flag) {}
+            chain->addTask(*secondTask);
+        };
+        *secondTask = []() {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        };
+        secondTaskWeak = secondTask.toWeakRef();
+        chain->addTask(*firstTask);
+    }
+    EXPECT_FALSE(chainWeak.isNull());
+    EXPECT_FALSE(secondTaskWeak.isNull());
+    flag = true;
+    for (int i = 0; i < 2000; ++i) {
+        if (chainWeak.isNull())
+            break;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+    thread.quit();
+    thread.wait(100);
+    EXPECT_TRUE(chainWeak.isNull());
+    EXPECT_TRUE(secondTaskWeak.isNull());
+}
+
+TEST(TaskChainTest, stepsPerforming)
+{
+    TaskChainWP chainWeak;
+    QThread thread;
+    thread.start();
+    std::atomic<int> counter(0);
+    std::atomic<bool> flag(false);
+    {
+        TaskChainSP chain = TaskChain::createChain();
+        chain->moveToThread(&thread);
+        chainWeak = chain.toWeakRef();
+        auto firstTask = TaskChain::createTask<>();
+        auto secondTask = TaskChain::createTask<>();
+        *firstTask = [&flag, chain, secondTask, &counter]() {
+            while (!flag) {}
+            ++counter;
+            chain->addTask(*secondTask);
+        };
+        *secondTask = [&counter]() {
+            ++counter;
+        };
+        chain->addTask(*firstTask);
+    }
+    EXPECT_FALSE(chainWeak.isNull());
+    flag = true;
+    for (int i = 0; i < 2000; ++i) {
+        if (chainWeak.isNull())
+            break;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+    thread.quit();
+    thread.wait(100);
+    EXPECT_TRUE(chainWeak.isNull());
+    EXPECT_EQ(2, counter);
+}
+
+TEST(TaskChainTest, stepsPerformingWithArgs)
+{
+    TaskChainWP chainWeak;
+    QThread thread;
+    thread.start();
+    std::atomic<int> counter(0);
+    std::atomic<bool> flag(false);
+    {
+        TaskChainSP chain = TaskChain::createChain();
+        chain->moveToThread(&thread);
+        chainWeak = chain.toWeakRef();
+        auto firstTask = TaskChain::createTask<>();
+        auto secondTask = TaskChain::createTask<std::atomic<int> *>();
+        *firstTask = [&flag, chain, secondTask, &counter]() {
+            while (!flag) {}
+            ++counter;
+            chain->addTask(*secondTask, &counter);
+        };
+        *secondTask = [](std::atomic<int> *counterPtr) {
+            ++(*counterPtr);
+        };
+        chain->addTask(*firstTask);
+    }
+    EXPECT_FALSE(chainWeak.isNull());
+    flag = true;
+    for (int i = 0; i < 2000; ++i) {
+        if (chainWeak.isNull())
+            break;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+    thread.quit();
+    thread.wait(100);
+    EXPECT_TRUE(chainWeak.isNull());
+    EXPECT_EQ(2, counter);
+}
