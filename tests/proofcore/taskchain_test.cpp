2949c9e58d0f5900321d1a5e33a82ff10224057f
Denis Kormalev <denis.kormalev@opensoftdev.com>
2014-07-21 16:28:56 +0400

    Task chain test done

--- tests/proofcore/taskchain_test.cpp
+++ tests/proofcore/taskchain_test.cpp
@@ -79,18 +79,23 @@ TEST(TaskChainTest, stepsPerforming)
     thread.start();
     std::atomic<int> counter(0);
     std::atomic<bool> flag(false);
+    auto mainThreadId = std::this_thread::get_id();
+    std::thread::id firstTaskThreadId;
+    std::thread::id secondTaskThreadId;
     {
         TaskChainSP chain = TaskChain::createChain();
         chain->moveToThread(&thread);
         chainWeak = chain.toWeakRef();
         auto firstTask = TaskChain::createTask<>();
         auto secondTask = TaskChain::createTask<>();
-        *firstTask = [&flag, chain, secondTask, &counter]() {
+        *firstTask = [&flag, chain, secondTask, &counter, &firstTaskThreadId]() {
             while (!flag) {}
+            firstTaskThreadId = std::this_thread::get_id();
             ++counter;
             chain->addTask(*secondTask);
         };
-        *secondTask = [&counter]() {
+        *secondTask = [&counter, &secondTaskThreadId]() {
+            secondTaskThreadId = std::this_thread::get_id();
             ++counter;
         };
         chain->addTask(*firstTask);
@@ -105,6 +110,9 @@ TEST(TaskChainTest, stepsPerforming)
     thread.quit();
     thread.wait(100);
     EXPECT_TRUE(chainWeak.isNull());
+    EXPECT_NE(mainThreadId, firstTaskThreadId);
+    EXPECT_NE(mainThreadId, secondTaskThreadId);
+    EXPECT_NE(firstTaskThreadId, secondTaskThreadId);
     EXPECT_EQ(2, counter);
 }
 
@@ -142,4 +150,50 @@ TEST(TaskChainTest, stepsPerformingWithArgs)
     thread.wait(100);
     EXPECT_TRUE(chainWeak.isNull());
     EXPECT_EQ(2, counter);
+}
+
+TEST(TaskChainTest, signalWaiting)
+{
+    TaskChainWP chainWeak;
+    QThread thread;
+    thread.start();
+    std::atomic<bool> flag(false);
+    std::atomic<bool> result(false);
+    std::atomic<bool> finished(false);
+    QTimer *timer = new QTimer();
+    timer = new QTimer;
+    timer->setSingleShot(true);
+    timer->moveToThread(&thread);
+    {
+        TaskChainSP chain = TaskChain::createChain();
+        chain->moveToThread(&thread);
+        chainWeak = chain.toWeakRef();
+        auto firstTask = TaskChain::createTask<>();
+        *firstTask = [&flag, chain, timer, &result, &finished]() {
+            std::function<bool()> timerCallback = [&result]() {
+                result = true;
+                return true;
+            };
+            chain->addSignalWaiter(timer, &QTimer::timeout, timerCallback);
+            flag = true;
+            chain->fireSignalWaiters();
+            delete timer;
+            finished = true;
+        };
+        chain->addTask(*firstTask);
+    }
+    EXPECT_FALSE(chainWeak.isNull());
+    while (!flag) {}
+    EXPECT_FALSE(finished);
+    QMetaObject::invokeMethod(timer, "start", Qt::BlockingQueuedConnection, Q_ARG(int, 100));
+    for (int i = 0; i < 2000; ++i) {
+        if (chainWeak.isNull())
+            break;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+    thread.quit();
+    thread.wait(100);
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(finished);
+    EXPECT_TRUE(chainWeak.isNull());
 }
