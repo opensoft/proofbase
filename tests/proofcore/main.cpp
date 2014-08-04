d9f93fae3be5b4b0bb6bde315a682f2fa344579b
Denis Kormalev <denis.kormalev@opensoftdev.com>
2014-08-04 13:56:35 +0400

    Test-runner fixes for linux and windows

--- tests/proofcore/main.cpp
+++ tests/proofcore/main.cpp
@@ -1,9 +1,9 @@
 #include "gtest/test_global.h"
 
-#include <QGuiApplication>
+#include <QCoreApplication>
 
 GTEST_API_ int main(int argc, char **argv) {
-    QGuiApplication app(argc, argv);
+    QCoreApplication app(argc, argv);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
