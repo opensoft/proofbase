a44e20008208b0d3c683dda5a7d1790b8d479964
Vasiliy Sorokin <vasiliy.sorokin@opensoftdev.ru>
2014-09-12 10:14:55 +0400

    Fill tables

--- src/proofcore/proofglobal.cpp
+++ src/proofcore/proofglobal.cpp
@@ -2,22 +2,22 @@
 
 namespace Proof {
 QString proofVersion() {
-    return "";//PROOF_VERSION;
+    return PROOF_VERSION;
 }
 
 int proofVersionMajor() {
-    return 0;//PROOF_VERSION_MAJOR;
+    return PROOF_VERSION_MAJOR;
 }
 
 int proofVersionYear() {
-    return 0;//PROOF_VERSION_YEAR;
+    return PROOF_VERSION_YEAR;
 }
 
 int proofVersionMonth() {
-    return 0;//PROOF_VERSION_MONTH;
+    return PROOF_VERSION_MONTH;
 }
 
 int proofVersionDay() {
-    return 0;//PROOF_VERSION_DAY;
+    return PROOF_VERSION_DAY;
 }
 }
