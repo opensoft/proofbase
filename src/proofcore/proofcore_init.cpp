69a2517c1854ac4bcb97360a4134505a8efaf313
Denis Kormalev <denis.kormalev@opensoftdev.com>
2014-10-24 16:41:42 -0400

    minor renaming

--- src/proofcore/proofcore_init.cpp
+++ src/proofcore/proofcore_init.cpp
@@ -4,7 +4,7 @@
 Q_LOGGING_CATEGORY(proofLog, "proof")
 Q_LOGGING_CATEGORY(proofCoreLog, "proof.core")
 Q_LOGGING_CATEGORY(proofCoreSettingsLog, "proof.core.settings")
-Q_LOGGING_CATEGORY(proofCoreTaskchainLog, "proof.core.taskchain")
+Q_LOGGING_CATEGORY(proofCoreTaskChainLog, "proof.core.taskchain")
 Q_LOGGING_CATEGORY(proofCoreCacheLog, "proof.core.cache")
 
 __attribute__((constructor))
