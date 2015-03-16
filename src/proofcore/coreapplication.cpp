08587eafbef003e45eaff031e683239c99945c09
Vasiliy Sorokin <vasiliy.sorokin@opensoftdev.ru>
2015-03-16 12:09:15 +0000

    Add more info tocrash reporter

--- src/proofcore/coreapplication.cpp
+++ src/proofcore/coreapplication.cpp
@@ -10,17 +10,30 @@
 #include <cxxabi.h>
 #include <execinfo.h>
 #include <signal.h>
+#include <sys/ucontext.h>
 #endif
 
 #ifdef Q_OS_LINUX
 constexpr int BACKTRACE_MAX_SIZE = 50;
-static void signalHandler(int sig)
+static struct sigaction sigAction;
+
+static void signalHandler(int sig, siginfo_t *info, void *context)
 {
+    ucontext_t *uc = (ucontext_t *)context;
+    void *caller = (void *) uc->uc_mcontext.fpregs->rip;
+
     qCCritical(proofCoreCrashLog) << "#######################################";
-    qCCritical(proofCoreCrashLog) << QString("signal %1 (%2)").arg(sig).arg(strsignal(sig));
+    qCCritical(proofCoreCrashLog) << QString("signal %1 (%2), address is 0x%3 from 0x%4")
+                                     .arg(sig)
+                                     .arg(strsignal(sig))
+                                     .arg((unsigned long long) info->si_addr, 0, 16)
+                                     .arg((unsigned long long) caller, 0, 16);
 
     void *backtraceInfo[BACKTRACE_MAX_SIZE];
     int size = backtrace(backtraceInfo, BACKTRACE_MAX_SIZE);
+
+    backtraceInfo[0] = backtraceInfo[1];
+    backtraceInfo[1] = caller;
 
     char **backtraceArray = backtrace_symbols(backtraceInfo, size);
 
@@ -37,11 +50,11 @@ static void signalHandler(int sig)
         } else {
             char *name = abi::__cxa_demangle(re.cap(2).trimmed().toLatin1().constData(), 0, 0, 0);
             qCCritical(proofCoreCrashLog) << QString("[trace] #%1) %2 : %3+%4 (%5)")
-                                                     .arg(i)
-                                                     .arg(re.cap(1).trimmed()) //scope
-                                                     .arg(name ? name : re.cap(2).trimmed()) //name
-                                                     .arg(re.cap(3).trimmed()) //offset
-                                                     .arg(re.cap(4).trimmed()); //address
+                                             .arg(i)
+                                             .arg(re.cap(1).trimmed()) //scope
+                                             .arg(name ? name : re.cap(2).trimmed()) //name
+                                             .arg(re.cap(3).trimmed()) //offset
+                                             .arg(re.cap(4).trimmed()); //address
             free(name);
         }
     }
@@ -92,8 +105,12 @@ void Proof::CoreApplicationPrivate::initApp()
     if (!logFileName.isEmpty())
         Logs::installFileHandler(logFileName);
 
-#ifdef Q_OS_LINUX
-    signal(SIGSEGV, signalHandler);
+#ifdef Q_OS_LINUX    
+    sigAction.sa_sigaction = signalHandler;
+    sigAction.sa_flags = SA_SIGINFO;
+
+    if (sigaction(SIGSEGV, &sigAction, (struct sigaction *)NULL) != 0)
+        qCWarning(proofCoreLoggerLog()) << "No segfault handler is on your back.";
 #endif
 
 }
