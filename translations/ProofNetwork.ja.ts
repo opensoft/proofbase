23d5d990767fc8286bcbafe7d9a3f05fddbfa551
Igor Ermakov <igor.ermakov@opensoftdev.ru>
2015-08-17 16:46:09 +0300

    .ts files

---
+++ translations/ProofNetwork.ja.ts
@@ -0,0 +1,216 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../abstractrestapi.cpp" line="213"/>
+        <source>Service is inaccessible. Try again later</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../abstractrestapi.cpp" line="256"/>
+        <location filename="../abstractrestapi_p.h" line="84"/>
+        <location filename="../abstractrestapi_p.h" line="205"/>
+        <location filename="../envelopepacks/envelopepacksapi.cpp" line="47"/>
+        <location filename="../fiery/fieryrestapi.cpp" line="14"/>
+        <location filename="../imposition/impositionapi.cpp" line="90"/>
+        <location filename="../imposition/impositionapi.cpp" line="117"/>
+        <location filename="../labelprinter/labelprinterapi.cpp" line="32"/>
+        <location filename="../labelprinter/labelprinterapi.cpp" line="56"/>
+        <location filename="../profit/ordersapi.cpp" line="168"/>
+        <location filename="../profit/presssheetitemsapi.cpp" line="155"/>
+        <source>JSON error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../abstractrestapi_p.h" line="112"/>
+        <source>Can&apos;t create list of entities from server response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../abstractrestapi_p.h" line="175"/>
+        <source>Can&apos;t create entity from server response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../abstractrestapi_p.h" line="216"/>
+        <source>JSON error: empty entity data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../boxbuilding/boxbuildingapi.cpp" line="46"/>
+        <location filename="../boxbuilding/boxbuildingapi.cpp" line="140"/>
+        <source>Can&apos;t create boxes info list: JSON document is not an array</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../boxbuilding/boxbuildingapi.cpp" line="61"/>
+        <location filename="../boxbuilding/boxbuildingapi.cpp" line="155"/>
+        <source>Can&apos;t create boxes info list.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../boxbuilding/boxbuildingapi.cpp" line="98"/>
+        <location filename="../boxbuilding/boxbuildingapi.cpp" line="198"/>
+        <source>Can&apos;t create box: JSON document is not an object.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../boxbuilding/boxbuildingapi.cpp" line="246"/>
+        <source>Can&apos;t search for done jobs: JSON document is not an array.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../envelopepacks/envelopepacksapi.cpp" line="54"/>
+        <location filename="../imposition/impositionapi.cpp" line="122"/>
+        <location filename="../labelprinter/labelprinterapi.cpp" line="36"/>
+        <location filename="../labelprinter/labelprinterapi.cpp" line="60"/>
+        <source>Object is not found in document</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../envelopepacks/envelopepacksapi.cpp" line="72"/>
+        <location filename="../imposition/impositionapi.cpp" line="133"/>
+        <source>Unknown server error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../envelopepacks/envelopepacksapi.cpp" line="80"/>
+        <location filename="../imposition/impositionapi.cpp" line="140"/>
+        <source>Required fields weren&apos;t found in response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fiery/jobsapi.cpp" line="91"/>
+        <source>Can&apos;t read file for create job request.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fiery/jobsapi.cpp" line="114"/>
+        <location filename="../fiery/queuesapi.cpp" line="81"/>
+        <source>Can&apos;t read id from server response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fiery/jobsapi.cpp" line="133"/>
+        <source>Can&apos;t delete job</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fiery/jobsapi.cpp" line="156"/>
+        <location filename="../fiery/jobsapi.cpp" line="186"/>
+        <location filename="../fiery/queuesapi.cpp" line="127"/>
+        <source>Can&apos;t read flag from server response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fiery/queuesapi.cpp" line="100"/>
+        <source>Can&apos;t delete queue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fiery/systemapi.cpp" line="57"/>
+        <source>No session cookie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fiery/systemapi.cpp" line="114"/>
+        <source>Can&apos;t read status from server response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fiery/systemapi.cpp" line="221"/>
+        <location filename="../fiery/systemapi.cpp" line="243"/>
+        <source>Can&apos;t read process status from server response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fiery/systemapi.cpp" line="306"/>
+        <source>Can&apos;t read localization from server response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fiery/systemapi.cpp" line="334"/>
+        <location filename="../fiery/systemapi.cpp" line="355"/>
+        <source>Can&apos;t read system pages from server response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../httpparser.cpp" line="93"/>
+        <source>Can&apos;t convert %1 to unsinged long long for &quot;Content-Length&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../httpparser.cpp" line="104"/>
+        <source>Invalid header: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imposition/impositionapi.cpp" line="47"/>
+        <source>Invalid JDF</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imposition/impositionapi.cpp" line="95"/>
+        <source>Can&apos;t create list of job identifiers from server response</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../profit/ordersapi.cpp" line="191"/>
+        <location filename="../profit/presssheetitemsapi.cpp" line="178"/>
+        <source>Received wrong data instead of boxes list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../profit/presssheetsapi.cpp" line="133"/>
+        <source>Can&apos;t create remaining boxes list: JSON document is not an array</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../profit/presssheetsapi.cpp" line="143"/>
+        <source>Can&apos;t create remaining boxes list: there is no &quot;type&quot; field</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../profit/presssheetsapi.cpp" line="145"/>
+        <source>Can&apos;t create remaining boxes list: there is no &quot;quantity&quot; field</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../profit/presssheetsapi.cpp" line="180"/>
+        <location filename="../profit/productboxesapi.cpp" line="125"/>
+        <source>Can&apos;t create WorkflowHistory from server response: JSON document is not an array and doesn&apos;t have message field</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../profit/profitrestapi.cpp" line="28"/>
+        <source>Errors: 
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../profit/shippingapi.cpp" line="158"/>
+        <source>No trucks found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../restclient.cpp" line="447"/>
+        <source>Can&apos;t connect to Scissorhands service.
+Please check your internet connection.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../restclient.cpp" line="460"/>
+        <source>Wrong Scissorhands service authentication.
+Please check your authentication settings.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../restclient.cpp" line="465"/>
+        <source>Wrong Scissorhands service answer.
+Please check your host settings.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
