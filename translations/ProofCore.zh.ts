1001a45fc8ce446e8a012a338c01619cedd0a6bf
Denis Kormalev <denis.kormalev@opensoftdev.com>
2017-04-26 20:26:14 +0300

    Empty chinese translations

---
+++ translations/ProofCore.zh.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../errormessagesregistry.cpp" line="23"/>
+        <source>Unknown error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
