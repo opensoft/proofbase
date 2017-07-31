786e9051fbb4d96f7b06fd9106904095b927f97b
Denis Kormalev <denis.kormalev@opensoftdev.com>
2017-07-31 16:03:22 -0400

    Proof::math and Proof::algorithms (#1518)

--- include/proofcore/proofglobal.h
+++ include/proofcore/proofglobal.h
@@ -13,6 +13,36 @@ PROOF_CORE_EXPORT int proofVersionDay();
 
 PROOF_CORE_EXPORT void setProofUsesSettings(bool value);
 PROOF_CORE_EXPORT bool proofUsesSettings();
+
+namespace math {
+static constexpr int round(double d)
+{
+#ifdef Q_CC_MSVC
+    return d >= 0.0 ? int(d + 0.5) : int(d - double(int(d - 1)) + 0.5) + int(d - 1);
+#else
+    return qRound(d);
+#endif
+}
+}
+
+namespace algorithms {
+template<typename Key, typename T, typename Predicate>
+void eraseIf(QMap<Key, T> &container, const Predicate &predicate)
+{
+    for (auto it = container.begin(); it != container.end();) {
+        if (predicate(it))
+            it = container.erase(it);
+        else
+            ++it;
+    }
+}
+
+template<typename Container, typename Predicate>
+void eraseIf(Container &container, const Predicate &predicate)
+{
+    container.erase(std::remove_if(container.begin(), container.end(), predicate), container.end());
+}
+}
 }
 
 #ifdef Q_CC_MSVC
