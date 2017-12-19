f600529bdcbb580cbd4e6597d37fc99b237b76a1
Denis Kormalev <denis.kormalev@opensoftdev.com>
2017-12-19 17:03:36 -0500

    MSVC2015 support fixed

--- include/proofcore/proofalgorithms.h
+++ include/proofcore/proofalgorithms.h
@@ -5,6 +5,39 @@
 
 namespace Proof {
 namespace algorithms {
+namespace __util {
+//TODO: remove this workaround with second void() after msvc 2015->2017 transition
+template<typename C, typename T>
+auto addToContainer(C &container, const T &value) -> decltype(container.push_back(value), void(), void())
+{
+    container.push_back(value);
+}
+
+template<typename C, typename T>
+auto addToContainer(C &container, const T &value) -> decltype(container.insert(value), void())
+{
+    container.insert(value);
+}
+
+template<typename C, typename T>
+auto addToContainer(C &container, const T &value) -> decltype(container.insert(value.key(), value.value()), void())
+{
+    container.insert(value.key(), value.value());
+}
+
+template<typename C, typename T>
+auto addToContainer(C &container, const T &value) -> decltype(container.insert(value.first, value.second), void())
+{
+    container.insert(value.first, value.second);
+}
+
+//TODO: remove this workaround with wrapper for const_cast after msvc 2015->2017 transition
+template<typename T> T & constCastWrapper(const T &ref)
+{
+    return const_cast<T &>(ref);
+}
+}
+
 template<typename Container, typename Predicate>
 auto eraseIf(Container &container, const Predicate &predicate)
 -> decltype(predicate(container.cbegin().key(), container.cbegin().value()),
@@ -60,37 +93,13 @@ auto findIf(const Container &container, const Predicate &predicate, const Result
     return defaultValue;
 }
 
-template<typename C, typename T>
-auto addToContainer(C &container, const T &value) -> decltype(container.push_back(value), void())
-{
-    container.push_back(value);
-}
-
-template<typename C, typename T>
-auto addToContainer(C &container, const T &value) -> decltype(container.insert(value), void())
-{
-    container.insert(value);
-}
-
-template<typename C, typename T>
-auto addToContainer(C &container, const T &value) -> decltype(container.insert(value.key(), value.value()), void())
-{
-    container.insert(value.key(), value.value());
-}
-
-template<typename C, typename T>
-auto addToContainer(C &container, const T &value) -> decltype(container.insert(value.first, value.second), void())
-{
-    container.insert(value.first, value.second);
-}
-
 // filter, map and reduce are not lazy, they copy values.
 // It is fine for almost all our cases with DTOs and PODs, but still is O(n).
 // O(n) though is good enough for our amounts of data.
 // If ever lazy variant will be needed probably we will need to use boost ranges or something similar then.
 template<typename Container, typename Predicate>
 auto filter(const Container &container, const Predicate &predicate)
--> decltype(addToContainer(const_cast<Container&>(container), *(container.cbegin())),
+-> decltype(__util::addToContainer(__util::constCastWrapper(container), *(container.cbegin())),
             predicate(*(container.cbegin())),
             Container())
 {
@@ -99,14 +108,14 @@ auto filter(const Container &container, const Predicate &predicate)
     auto end = container.cend();
     for (; it != end; ++it) {
         if (predicate(*it))
-            addToContainer(result, *it);
+            __util::addToContainer(result, *it);
     }
     return result;
 }
 
 template<typename Container, typename Predicate>
 auto filter(const Container &container, const Predicate &predicate)
--> decltype(addToContainer(const_cast<Container&>(container), container.cbegin()),
+-> decltype(__util::addToContainer(__util::constCastWrapper(container), container.cbegin()),
             predicate(container.cbegin().key(), container.cbegin().value()),
             Container())
 {
@@ -115,32 +124,32 @@ auto filter(const Container &container, const Predicate &predicate)
     auto end = container.cend();
     for (; it != end; ++it) {
         if (predicate(it.key(), it.value()))
-            addToContainer(result, it);
+            __util::addToContainer(result, it);
     }
     return result;
 }
 
 template<typename Container, typename Predicate, class Result>
 auto map(const Container &container, const Predicate &predicate, Result destination)
--> decltype(addToContainer(destination, predicate(*(container.cbegin()))),
+-> decltype(__util::addToContainer(destination, predicate(*(container.cbegin()))),
             Result())
 {
     auto it = container.cbegin();
     auto end = container.cend();
     for (; it != end; ++it)
-        addToContainer(destination, predicate(*it));
+        __util::addToContainer(destination, predicate(*it));
     return destination;
 }
 
 template<typename Container, typename Predicate, class Result>
 auto map(const Container &container, const Predicate &predicate, Result destination)
--> decltype(addToContainer(destination, predicate(container.cbegin().key(), container.cbegin().value())),
+-> decltype(__util::addToContainer(destination, predicate(container.cbegin().key(), container.cbegin().value())),
             Result())
 {
     auto it = container.cbegin();
     auto end = container.cend();
     for (; it != end; ++it)
-        addToContainer(destination, predicate(it.key(), it.value()));
+        __util::addToContainer(destination, predicate(it.key(), it.value()));
     return destination;
 }
 
