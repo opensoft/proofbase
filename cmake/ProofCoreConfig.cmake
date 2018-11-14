16ab4c37e56e68d0ea549f455a812e9d0193c603
Denis Kormalev <denis.kormalev@opensoftdev.com>
2018-11-14 11:07:42 -0800

    zlib dependency added to ProofCoreConfig.cmake

--- cmake/ProofCoreConfig.cmake
+++ cmake/ProofCoreConfig.cmake
@@ -1,6 +1,7 @@
 include(CMakeFindDependencyMacro)
 
 list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}/3rdparty")
+find_dependency(ZLIB REQUIRED)
 find_dependency(Qt5Core CONFIG REQUIRED)
 find_dependency(Qca-qt5 CONFIG REQUIRED)
 find_dependency(ProofSeed CONFIG REQUIRED)
