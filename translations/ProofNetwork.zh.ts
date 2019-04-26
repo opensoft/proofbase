9aed4dce9ab72aa9b2063758cbb0f486e1d6aee5
Dmitry Nesterenok <nesterenokda@gmail.com>
2019-04-26 18:38:04 +0400

    Added Russian translation (#26)

--- translations/ProofNetwork.zh.ts
+++ translations/ProofNetwork.zh.ts
@@ -1,4 +1,31 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Unknown error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Host %1 is unavailable. Try again later</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You don&apos;t have network connection. Please, connect your device to network and try again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Your device seems to be in network without Internet connection. Please, check if Internet is accessible and try again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Host %1 not found. Please, contact IT immediately.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Slow network access to %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
